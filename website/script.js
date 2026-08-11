25b06e5017dea3ad3ee8643f84110981a5edd2f1
kousumi04 <kousumimondal2004@gmail.com>
2026-08-12 00:41:54 +0530

    pfp functionality added

--- website/script.js
+++ website/script.js
@@ -3,6 +3,7 @@ const uploadDialog = document.querySelector(".upload-dialog");
 const uploadForm = document.querySelector(".upload-form");
 const fileInput = document.querySelector('.file-field input[type="file"]');
 const cancelButton = document.querySelector(".cancel-button");
+const uploadedPhoto = document.querySelector(".uploaded-photo");
 
 scooterButton?.addEventListener("click", () => {
   if (typeof uploadDialog?.showModal === "function") {
@@ -25,6 +26,29 @@ uploadForm?.addEventListener("submit", (event) => {
     return;
   }
 
-  uploadDialog?.close();
-  scooterButton?.classList.add("is-exiting");
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    sessionStorage.setItem("profilePhoto", String(reader.result));
+    uploadDialog?.close();
+    scooterButton?.classList.add("is-exiting");
+  });
+
+  reader.readAsDataURL(fileInput.files[0]);
 });
+
+scooterButton?.addEventListener("animationend", (event) => {
+  if (event.animationName === "scooter-exit") {
+    window.location.href = "agenda.html";
+  }
+});
+
+if (uploadedPhoto) {
+  const profilePhoto = sessionStorage.getItem("profilePhoto");
+
+  if (profilePhoto) {
+    uploadedPhoto.src = profilePhoto;
+  } else {
+    uploadedPhoto.remove();
+  }
+}
